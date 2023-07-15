0fcadc65fa641105be9c20de6c2b2866a8cd764f
Elnaz <elnazmalakouti@gmail.com>
2023-07-15 22:30:53 +0330

    calculate age with error handling

--- src/Components/CalculatorForm/index.jsx
+++ src/Components/CalculatorForm/index.jsx
@@ -5,52 +5,151 @@ const CalculatorForm = () => {
 
     const [inputErrorMode, setInputErrorMode] = useState(false)
 
+    const [dayErrorMessage, setDayErrorMesaage] = useState('')
+    const [monthErrorMessage, setMonthErrorMessage] = useState('')
+    const [yearErrorMessage, setYearErrorMessage] = useState('')
+
+    const [enteredDayValue, setEnteredDayValue] = useState(null)
+    const [enteredMonthValue, setEnteredMonthValue] = useState(null)
+    const [enteredYearValue, setEnteredYearValue] = useState(null)
+
+    const [dayResult, setDayResult] = useState()
+    const [monthResult, setMonthResult] = useState()
+    const [yearResult, setYearResult] = useState()
+
+    const months31days = ['1', '3', '5', '7', '8', '10', '12']
+    const month30days = [4, 6, 9, 11]
+
+    const moment = require('moment');
+
+    function calculateAge(birthDate) {
+        checkEnteredValues()
+        const today = moment();
+        const birth = moment(birthDate, 'YYYY-MM-DD');
+
+        const ageDuration = moment.duration(today.diff(birth));
+        // if (ageDuration.isValid()) {
+        //     setDayErrorMesaage(null)
+        //     setMonthErrorMessage(null)
+        //     setYearErrorMessage(null)
+        // }
+        const years = ageDuration.years() > 0 ? ageDuration.years() : '--';
+        const months = ageDuration.months() > 0 ? ageDuration.months() : '--';
+        const days = ageDuration.days() > 0 ? ageDuration.days() : '--';
+
+        setYearResult(years)
+        setMonthResult(months)
+        setDayResult(days)
+    }
+
+    const checkEnteredValues = () => {
+        const now = new Date();
+        const currentYear = now.getFullYear();
+
+
+        if (enteredDayValue == null) {
+            setDayErrorMesaage('This field is required')
+        }
+
+        if (enteredDayValue == 31) {
+            if (!(months31days.includes(enteredMonthValue))) {
+                setDayErrorMesaage('Must be a valid date')
+            }
+        } else if (enteredDayValue < 1 || enteredDayValue > 31) {
+            setDayErrorMesaage('Must be a valid day')
+        } else {
+            setDayErrorMesaage(null)
+        }
+
+
+
+
+
+        if (enteredMonthValue == null) {
+            setMonthErrorMessage('This field is required')
+        } else if (enteredMonthValue < 1 || enteredMonthValue > 12) {
+            setMonthErrorMessage('Must be a valid month')
+        } else {
+            setMonthErrorMessage(null)
+        }
+
+
+
+
+
+        if (enteredYearValue == null) {
+            setYearErrorMessage('This field is required')
+        }
+
+        if (enteredYearValue > currentYear) {
+            setYearErrorMessage('Must be in the past')
+        } else {
+            setYearErrorMessage(null)
+        }
+
+        if ((+enteredYearValue % 4 == 0 && +enteredYearValue % 100 !== 0) || +enteredYearValue % 400 == 0) {
+            if ((enteredMonthValue == 2) && (enteredDayValue == 30 || enteredDayValue == 31)) {
+                setDayErrorMesaage('Must be a valid date')
+            }
+        } else if ((enteredMonthValue == 2) && (enteredDayValue == 29 || enteredDayValue == 30 || enteredDayValue == 31)) {
+            setDayErrorMesaage('Must be a valid date')
+        }
+
+
+    }
+
     return (
         <>
             <div className="w-full md:w-[65%] xl:w-[45%] h-auto bg-white px-4 py-8 rounded-2xl rounded-br-[5rem] flex flex-col gap-12 2xl:gap-20 md:gap-8 justify-center md:justify-start items-center md:items-start">
 
 
-                <div className="w-full md:w-auto md:mr-auto flex justify-between md:justify-center items-center gap-2 2xl:gap-8">
+                <div className="w-full md:w-auto md:mr-auto flex justify-between md:justify-center items-center gap-2 lg:gap-4 2xl:gap-8">
                     <Input
                         lableText={'Day'}
                         palceholder={'DD'}
-                        inputErrorMode={inputErrorMode}
-                        setInputErrorMode={setInputErrorMode}
-                        errorMessage={'Must be a valid day'}
+                        errorMessage={dayErrorMessage}
+                        setErrorMessage={setDayErrorMesaage}
+                        value={enteredDayValue}
+                        onChange={(e) => setEnteredDayValue(e.target.value)}
                     />
                     <Input
                         lableText={'Month'}
                         palceholder={'MM'}
-                        inputErrorMode={inputErrorMode}
-                        setInputErrorMode={setInputErrorMode}
-                        errorMessage={'Must be a valid month'}
+                        errorMessage={monthErrorMessage}
+                        setErrorMessage={setMonthErrorMessage}
+                        value={enteredMonthValue}
+                        onChange={(e) => setEnteredMonthValue(e.target.value)}
                     />
                     <Input
                         lableText={'Year'}
                         palceholder={'YYYY'}
-                        inputErrorMode={inputErrorMode}
-                        setInputErrorMode={setInputErrorMode}
-                        errorMessage={'Must be in the past'}
+                        errorMessage={yearErrorMessage}
+                        setErrorMessage={setYearErrorMessage}
+                        value={enteredYearValue}
+                        onChange={(e) => setEnteredYearValue(e.target.value)}
                     />
                 </div>
 
                 <div className="w-full h-0.5 2xl:h-2 bg-[#EAEAEA]">
-                    <button className="w-12 h-12 2xl:w-24 2xl:h-24 rounded-full bg-[#864CFF] translate-y-[-50%] mx-auto md:mr-0 md:ml-auto flex justify-center items-center">
-                        <img alt="arrow-icon" src="/pics/icon-arrow.svg" className="w-6 h-6 2xl:w-12 2xl:h-12"/>
+                    <button
+                        onClick={() => calculateAge(`${enteredYearValue}-${enteredMonthValue}-${enteredDayValue}`)}
+                        className="w-12 h-12 2xl:w-24 2xl:h-24 rounded-full bg-[#864CFF] hover:bg-[#151515] translate-y-[-50%] mx-auto md:mr-0 md:ml-auto flex justify-center items-center"
+                    >
+                        <img alt="arrow-icon" src="/pics/icon-arrow.svg" className="w-6 h-6 2xl:w-12 2xl:h-12" />
                     </button>
                 </div>
 
                 <div className="flex flex-col mr-auto font-[Bold-italic] text-[3rem] md:text-[4rem]">
                     <p className="flex gap-1 my-[-0.5rem]">
-                        <span className="text-[#864CFF]">--</span>
+                        <span className="text-[#864CFF]">{yearResult ? yearResult : '--'}</span>
                         <span>years</span>
                     </p>
                     <p className="flex gap-1 my-[-0.5rem]">
-                        <span className="text-[#864CFF]">--</span>
+                        <span className="text-[#864CFF]">{monthResult ? monthResult : '--'}</span>
                         <span>months</span>
                     </p>
                     <p className="flex gap-1 my-[-0.5rem]">
-                        <span className="text-[#864CFF]">--</span>
+                        <span className="text-[#864CFF]">{dayResult ? dayResult : '--'}</span>
                         <span>days</span>
                     </p>
                 </div>
--- src/Components/Input/index.jsx
+++ src/Components/Input/index.jsx
@@ -1,10 +1,15 @@
-const Input = ({ lableText, palceholder, inputErrorMode, setInputErrorMode , errorMessage }) => {
+const Input = ({ lableText, palceholder, errorMessage, setErrorMessage , value, onChange }) => {
     return (
         <>
             <div className="flex flex-col justify-start items-start gap-2">
-                <label className={`${inputErrorMode ? 'text-red-500' : 'text-[#6E6E6E]'} text-xs 2xl:text-2xl font-[Bold]`}>{lableText}</label>
-                <input placeholder={palceholder} className={`${inputErrorMode ? 'border-red-700' : ''} w-full max-w-[7rem] 2xl:max-w-[10rem] h-12 2xl:h-16 px-2 rounded-lg border text-[#8B8B8B] text-lg 2xl:text-3xl font-[Bold] focus:outline-[#7765A6]`} />
-                <p className={`${inputErrorMode ? 'block' : 'hidden'} text-[11px] text-red-500`}>{errorMessage}</p>
+                <label className={`${errorMessage ? 'text-red-500' : 'text-[#6E6E6E]'} text-xs 2xl:text-2xl font-[Bold]`}>{lableText}</label>
+                <input
+                    value={value}
+                    onChange={onChange}
+                    placeholder={palceholder}
+                    className={`${errorMessage ? 'border-red-700' : ''} w-full max-w-[7rem] 2xl:max-w-[10rem] h-12 2xl:h-16 px-2 rounded-lg border text-[#8B8B8B] text-lg 2xl:text-3xl font-[Bold] focus:outline-[#7765A6]`}
+                />
+                <p className={`h-4 text-[11px] text-red-500`}>{errorMessage}</p>
             </div>
         </>
     )
